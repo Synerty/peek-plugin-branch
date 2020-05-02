d4a7115364d2d6c498cda6d7ac218c68ae1e8157
Jarrod Chesney <jarrod.chesney@synerty.com>
2020-05-02 16:36:08 +1000

    PEEK-539: imp: Updated publish.sh, added gitlab+sonar, fixed version strings
    Added .editorconfig
    Removed GITHUB push

--- peek_plugin_branch/__init__.py
+++ peek_plugin_branch/__init__.py
@@ -4,7 +4,7 @@ from typing import Type
 from peek_plugin_base.client.PluginClientEntryHookABC import PluginClientEntryHookABC
 from peek_plugin_base.server.PluginServerEntryHookABC import PluginServerEntryHookABC
 
-__version__ = '###PEEKVER###'
+__version__ = '0.0.0'
 
 
 def peekServerEntryHook() -> Type[PluginServerEntryHookABC]:
