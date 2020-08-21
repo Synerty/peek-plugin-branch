0459d6deca30dd7be57dbd5d13015898397e9f7a
Jacob Sorrell <jacob.sorrell@synerty.com>
2020-08-21 11:02:02 +1000

    PEEK-662: Replaced Font Awesome icons with Ant Design icons.

--- peek_plugin_branch/_private/both-app/branch.module.ts
+++ peek_plugin_branch/_private/both-app/branch.module.ts
@@ -1,107 +1,88 @@
-import {CommonModule} from "@angular/common";
-import {NgModule} from "@angular/core";
-import {Routes} from "@angular/router";
-
-// Import a small abstraction library to switch between nativescript and web
-import {PeekModuleFactory} from "@synerty/peek-util-web";
-
-// Import the default route component
-import {BranchComponent} from "./branch.component";
-
-// Import the required classes from VortexJS
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { Routes } from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { NzIconModule } from "ng-zorro-antd/icon";
+import { RouterModule } from "@angular/router";
+import { BranchComponent } from "./branch.component";
+import { branchTupleOfflineServiceName } from "@peek/peek_plugin_branch/_private/PluginNames";
 import {
     TupleOfflineStorageNameService,
-    TupleOfflineStorageService
-} from "@synerty/vortexjs";
-
-// Import the names we need for the
-import {
-    branchTupleOfflineServiceName
-} from "@peek/peek_plugin_branch/_private/PluginNames";
-
-// Import the required classes from VortexJS
-import {
+    TupleOfflineStorageService,
     TupleDataObservableNameService,
     TupleDataObserverService,
-    TupleDataOfflineObserverService
-} from "@synerty/vortexjs";
-
-// Import the names we need for the
-
-import {BranchDetailComponent} from "./branch-detail/branch-detail.component";
-
-import {
-    branchObservableName,
-    branchFilt
-} from "@peek/peek_plugin_branch/_private/PluginNames";
-
-// Import the required classes from VortexJS
-import {
+    TupleDataOfflineObserverService,
     TupleActionPushNameService,
     TupleActionPushOfflineService,
-    TupleActionPushService
+    TupleActionPushService,
 } from "@synerty/vortexjs";
-
-// Import the names we need for the
+import { BranchDetailComponent } from "./branch-detail/branch-detail.component";
 import {
-    branchActionProcessorName
-} from "@peek/peek_plugin_branch/_private";
-
+    branchObservableName,
+    branchFilt,
+} from "@peek/peek_plugin_branch/_private/PluginNames";
+import { branchActionProcessorName } from "@peek/peek_plugin_branch/_private";
 
 export function tupleActionPushNameServiceFactory() {
     return new TupleActionPushNameService(
-        branchActionProcessorName, branchFilt);
+        branchActionProcessorName,
+        branchFilt
+    );
 }
 
 export function tupleDataObservableNameServiceFactory() {
-    return new TupleDataObservableNameService(
-        branchObservableName, branchFilt);
+    return new TupleDataObservableNameService(branchObservableName, branchFilt);
 }
 
 export function tupleOfflineStorageNameServiceFactory() {
     return new TupleOfflineStorageNameService(branchTupleOfflineServiceName);
 }
 
-// Define the child routes for this plugin
+// Define the child routes for this plugin.
 export const pluginRoutes: Routes = [
     {
-        path: 'branchdetails',
-        component: BranchDetailComponent
+        path: "branchdetails",
+        component: BranchDetailComponent,
     },
     {
-        path: '',
-        pathMatch: 'full',
-        component: BranchComponent
-    }
-
+        path: "",
+        pathMatch: "full",
+        component: BranchComponent,
+    },
 ];
 
 // Define the root module for this plugin.
 // This module is loaded by the lazy loader, what ever this defines is what is started.
-// When it first loads, it will look up the routs and then select the component to load.
+// When it first loads, it will look up the routes and then select the component to load.
 @NgModule({
     imports: [
         CommonModule,
-        PeekModuleFactory.RouterModule,
-        PeekModuleFactory.RouterModule.forChild(pluginRoutes),
-        ...PeekModuleFactory.FormsModules,
+        RouterModule.forChild(pluginRoutes),
+        FormsModule,
+        NzIconModule,
+        HttpClientModule,
     ],
     exports: [],
     providers: [
-        TupleActionPushOfflineService, TupleActionPushService, {
+        TupleActionPushOfflineService,
+        TupleActionPushService,
+        {
             provide: TupleActionPushNameService,
-            useFactory: tupleActionPushNameServiceFactory
+            useFactory: tupleActionPushNameServiceFactory,
         },
-        TupleOfflineStorageService, {
+        TupleOfflineStorageService,
+        {
             provide: TupleOfflineStorageNameService,
-            useFactory: tupleOfflineStorageNameServiceFactory
+            useFactory: tupleOfflineStorageNameServiceFactory,
         },
-        TupleDataObserverService, TupleDataOfflineObserverService, {
+        TupleDataObserverService,
+        TupleDataOfflineObserverService,
+        {
             provide: TupleDataObservableNameService,
-            useFactory: tupleDataObservableNameServiceFactory
+            useFactory: tupleDataObservableNameServiceFactory,
         },
     ],
-    declarations: [BranchComponent, BranchDetailComponent]
+    declarations: [BranchComponent, BranchDetailComponent],
 })
-export class BranchModule {
-}
+export class BranchModule {}
